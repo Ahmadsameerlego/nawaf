e9fbfbd1f1266e34916542f05389dce2a0c07ec3
ahmadsamerlego@gmail.com <ahmadsamerlego@gmail.com>
2023-01-23 12:05:25 +0200

    'first'

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div>home</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { defineComponent } from 'vue';
 
-export default {
+
+export default defineComponent({
   name: 'HomeView',
+  data(){
+    return{
+    }
+  },
+
   components: {
-    HelloWorld
-  }
-}
+  },
+});
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  pluginOptions: {
+    vuetify: {},
+    i18n: {
+      locale: 'ar',
+      fallbackLocale: 'ar',
+      localeDir: 'locales',
+      enableLegacy: true,
+      runtimeOnly: false,
+      compositionOnly: true,
+      fullInstall: true
+    }
+  }
 })
